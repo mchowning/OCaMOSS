b0afc9715f225a991782bb11e945a3eba9b93d2a
Matt Chowning <mchowning@gmail.com>
2022-01-28 22:29:22 -0500

    Initial collection of matches into nested hashtables

--- lib/analysis.ml
+++ lib/analysis.ml
@@ -3,11 +3,14 @@ type hashLocation = {
   line: int;
 }
 
-type hashMatch = {
-  needle_path: string;
+type locations = {
   needle_line: int;
-  haystack_path: string;
   haystack_line: int;
+}
+
+type hashMatch = {
+  needle: hashLocation;
+  haystack: hashLocation;
 }
 
 let convertToHashDict fileHashDict = begin
@@ -36,15 +39,46 @@ let find_matches needles haystack = begin
         (* Record matching hashes in the haystack *)
         | Some ls -> 
             List.map (fun haystack_location -> 
-              { needle_path; 
-                needle_line = fingerprint.location; 
-                haystack_path = haystack_location.path; 
-                haystack_line = haystack_location.line }
+              { needle = { path = needle_path; line = fingerprint.location }; 
+                haystack = { path = haystack_location.path; line = haystack_location.line };
+              }
             ) ls
 
     ) needle_fingerprints in
     acc @ matches
   ) needles [];
+end
+
+let hash_matches_to_table (hms: hashMatch list) = begin
+
+  let tbl = Hashtbl.create (List.length hms) in
+  Base.List.iter hms ~f:(fun hm -> begin
+      let tbl' = match Hashtbl.find_opt tbl hm.needle.path with
+        | None -> Hashtbl.create 5
+        | Some t -> begin
+              Hashtbl.remove tbl hm.needle.path;
+              t
+            end
+      in
+
+      let locations = {
+        needle_line = hm.needle.line; 
+        haystack_line = hm.haystack.line 
+      } in
+
+      (match Hashtbl.find_opt tbl' hm.haystack.path with
+        | None -> begin
+              Hashtbl.add tbl' hm.haystack.path [ locations ];
+            end;
+        | Some ls -> begin
+            Hashtbl.remove tbl' hm.haystack.path;
+            Hashtbl.add tbl' hm.haystack.path (locations :: ls);
+          end);
+      
+      Hashtbl.add tbl hm.needle.path tbl'; 
+      end
+  );
+  tbl
 end
 
 let by_file hash_locations = begin
--- test/test_analysis.ml
+++ test/test_analysis.ml
@@ -2,26 +2,28 @@ open OUnit2
 open OCaMossLib
 
 
+let location_to_string (l: Analysis.locations) = 
+  "{ " ^ Int.to_string l.needle_line ^ "; " ^ Int.to_string l.haystack_line ^ " }"
 let list_printer l_printer ls =
-  let strings = List.map (fun x -> "    " ^ l_printer x) ls in
-  let items = String.concat ",\n" strings in
+  let strings = List.map l_printer ls in
+  let items = String.concat ";\n" strings in
   "[\n" ^ items ^ "\n]"
 
-let hashtbl_val_int_to_str tbl = Hashtbl.fold (fun a b acc -> begin
-  Hashtbl.add acc a (list_printer Int.to_string b);
+let hashtbl_val_to_str to_string tbl = Hashtbl.fold (fun a b acc -> begin
+  Hashtbl.add acc a (to_string b);
   acc
 end) tbl (Hashtbl.create 10)
 
 let hashtbl_printer tbl = 
   Hashtbl.fold (fun key value acc ->
-    acc ^ "\n  " ^ key ^ ": " ^ value
+    acc ^ "\n" ^ key ^ ": " ^ value
   ) tbl "\n{" |> (fun s -> s ^ "\n}\n")
 
 let hashMatch_printer (hm: Analysis.hashMatch) =
-  "{ " ^ "needle_path: " ^ hm.needle_path ^ ", " 
-       ^ "needle_hash: " ^ Int.to_string hm.needle_line ^ ", " 
-       ^ "haystack_path: " ^ hm.haystack_path ^ ", "
-       ^ "haystack_hash: " ^ Int.to_string hm.haystack_line ^
+  "{ " ^ "needle_path: " ^ hm.needle.path ^ ", " 
+       ^ "needle_hash: " ^ Int.to_string hm.needle.line ^ ", " 
+       ^ "haystack_path: " ^ hm.haystack.path ^ ", "
+       ^ "haystack_hash: " ^ Int.to_string hm.haystack.line ^
   " }"
 
 let matches_needles _ =
@@ -60,29 +62,70 @@ let matches_needles _ =
   let actual = Analysis.find_matches input_needles input_haystack in
 
   let expected: Analysis.hashMatch list = [
-    { needle_path = "a"; 
-      needle_line = 1; 
-      haystack_path = "aa"; 
-      haystack_line = 10 };
-
-    { needle_path = "c"; 
-      needle_line = 1; 
-      haystack_path = "aa"; 
-      haystack_line = 10 };
-    { needle_path = "c"; 
-      needle_line = 2; 
-      haystack_path = "bb"; 
-      haystack_line = 20 };
-    { needle_path = "c"; 
-      needle_line = 3; 
-      haystack_path = "aa"; 
-      haystack_line = 30 };
+    { needle = { path = "a"; line = 1; };
+      haystack = { path = "aa"; line = 10 };};
+    { needle = { path = "c"; line = 1; };
+      haystack = { path = "aa"; line = 10 };};
+    { needle = { path = "c"; line = 2; };
+      haystack = { path = "bb"; line = 20 };};
+    { needle = { path = "c"; line = 3; };
+      haystack = { path = "aa"; line = 30 };};
   ] in
 
   assert_equal ~printer:(list_printer hashMatch_printer) expected actual
 
+let hash_matches_to_table _ =
 
+  let (input: Analysis.hashMatch list) = [
+    { needle = { path = "a"; line = 1; };
+      haystack = { path = "aa"; line = 10 };};
+    { needle = { path = "c"; line = 1; };
+      haystack = { path = "aa"; line = 10 };};
+    { needle = { path = "c"; line = 2; };
+      haystack = { path = "bb"; line = 20 };};
+    { needle = { path = "c"; line = 1; };
+      haystack = { path = "bb"; line = 30 };};
+    { needle = { path = "c"; line = 3; };
+      haystack = { path = "aa"; line = 40 };};
+  ] in
+  let actual = Analysis.hash_matches_to_table input in
 
+  let expected = begin
+    let tbl = Hashtbl.create 5 in
+
+    let subTbl_a = begin
+      let subTbl = Hashtbl.create 5 in
+      let (value: Analysis.locations list) = [
+        { needle_line = 1; haystack_line = 10 };
+      ] in
+      Hashtbl.add subTbl "aa" value;
+      subTbl
+    end in
+    Hashtbl.add tbl "a" subTbl_a;
+
+    let subTbl_c = begin
+      let subTbl = Hashtbl.create 1 in
+      let (value: Analysis.locations list) = [ 
+        { needle_line = 3; haystack_line = 40 }; 
+        { needle_line = 1; haystack_line = 10 }; 
+      ] in
+      Hashtbl.add subTbl "aa" value;
+
+      let (value: Analysis.locations list) = [ 
+        { needle_line = 1; haystack_line = 30 }; 
+        { needle_line = 2; haystack_line = 20 }; 
+      ] in
+      Hashtbl.add subTbl "bb" value;
+      subTbl
+    end in
+    Hashtbl.add tbl "c" subTbl_c;
+
+    tbl
+  end in
+
+  let inner_tbl_printer tbl = hashtbl_printer (hashtbl_val_to_str (list_printer location_to_string) tbl) in
+  let printer tbl = hashtbl_printer (hashtbl_val_to_str inner_tbl_printer tbl) in
+  assert_equal ~printer:printer expected actual
 
 let results_by_file _ =
 
@@ -113,10 +156,11 @@ let results_by_file _ =
       tbl
     end in
 
-  assert_equal ~printer:(fun tbl -> hashtbl_printer (hashtbl_val_int_to_str tbl)) expected actual
+  assert_equal ~printer:(fun tbl -> hashtbl_printer (hashtbl_val_to_str (list_printer Int.to_string) tbl)) expected actual
  
 
 let tests = [
   "matches needles" >:: matches_needles;
   "results by file" >:: results_by_file;
+  "haystack matches to table" >:: hash_matches_to_table;
 ]
